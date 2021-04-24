282668c52a9c33000fa014139895cbac64f73a13
Gorges <kyle.gorges@kochind.com>
2021-04-24 00:00:32 -0500

    feat: team summary view

--- src/views/team/team-summary-view.dto.test.ts
+++ src/views/team/team-summary-view.dto.test.ts
@@ -3,8 +3,30 @@ import { TeamSummaryView } from '.';
 describe('given: team summary data', () => {
   describe('when: I create a view', () => {
     it('then: TeamSummaryView was returned', async () => {
-      const view = new TeamSummaryView().init({ id: 'jest.id' });
-      expect(view).toEqual({ id: 'jest.id' });
+      const view = new TeamSummaryView().init({
+        id: 'jest.id',
+        sequence: '0',
+        city: 'jest.city',
+        state: 'jest.state',
+        country: 'jest.country',
+        nickname: 'jest.nickname',
+        abbreviation: 'jest.abbreviation',
+        pluralNickname: 'jest.pluralNickname',
+        shortNickname: 'jest.shortNickname',
+        ownerUsername: 'jest.ownerUsername',
+      });
+      expect(view).toEqual({
+        id: 'jest.id',
+        sequence: '0',
+        city: 'jest.city',
+        state: 'jest.state',
+        country: 'jest.country',
+        nickname: 'jest.nickname',
+        abbreviation: 'jest.abbreviation',
+        pluralNickname: 'jest.pluralNickname',
+        shortNickname: 'jest.shortNickname',
+        ownerUsername: 'jest.ownerUsername',
+      });
     });
   });
 });
--- src/views/team/team-summary-view.dto.ts
+++ src/views/team/team-summary-view.dto.ts
@@ -4,4 +4,31 @@ import { Dto } from '../../dto';
 export class TeamSummaryView extends Dto {
   @IsString()
   id: string;
+
+  @IsString()
+  sequence: string;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  state: string;
+
+  @IsString()
+  country: string;
+
+  @IsString()
+  nickname: string;
+
+  @IsString()
+  abbreviation: string;
+
+  @IsString()
+  pluralNickname: string;
+
+  @IsString()
+  shortNickname: string;
+
+  @IsString()
+  ownerUsername: string;
 }
